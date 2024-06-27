9b4b0e9b62b8b8f812f77278634fd3085636c683
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-06-28 00:34:01 +0700

    add ui Tests

---
+++ src/test/java/tests/ui/LoginTests.java
@@ -0,0 +1,37 @@
+package tests.ui;
+
+import org.junit.jupiter.api.*;
+
+import static data.enums.Language.ru;
+import static io.qameta.allure.Allure.step;
+
+@Tag("ui")
+public class LoginTests extends TestBase {
+
+    @Test
+    void checkLoginTest() {
+        step("Открыть страницу", () -> greetingPages.openPage(String.valueOf(ru)));
+        step("Нажать кнопку Войти", () -> greetingPages.pressLoginButton());
+        step("Ввести логин", () -> accountsPage.enterLogin(data.USERNAME));
+        step("Ввести пароль", () -> accountsPage.enterPassword(data.PASSWORD));
+        step("Проверить имя компании на главной страницу", () -> mainPage.checkCompanyName(data.NAME_COMPANY));
+    }
+
+    @Test
+    void checkIncorrectPasswordTest() {
+        step("Открыть страницу", () -> greetingPages.openPage(String.valueOf(ru)));
+        step("Нажать кнопку Войти", () -> greetingPages.pressLoginButton());
+        step("Ввести логин", () -> accountsPage.enterLogin(data.USERNAME));
+        step("Ввести пароль", () -> accountsPage.enterPassword(data.INCORRECT_INPASSWORD));
+        step("Проверить имя компании на главной страницу", () -> accountsPage.checkingIncorrectPassword(data.INCORRECT_INPASSWORD));
+    }
+
+    @Test
+    void checkNameCompanyTest() {
+        step("Открыть страницу", () -> greetingPages.openPage(String.valueOf(ru)));
+        step("Нажать кнопку Войти", () -> greetingPages.pressLoginButton());
+        step("Ввести логин", () -> accountsPage.enterLogin(data.USERNAME));  // TODO переделать на некорректный логин
+        step("Ввести пароль", () -> accountsPage.enterPassword(data.PASSWORD));
+        step("Проверить имя компании на главной страницу", () -> mainPage.checkCompanyName(data.NAME_COMPANY));
+    }
+}
