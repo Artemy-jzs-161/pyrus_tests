9d35a35ba4346e4e88edceef7f7300ad82743751
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-06-30 22:51:01 +0700

    Удалил всё не нужное

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -40,13 +40,6 @@ public class Attach {
     }
 
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-    public static String addVideo(String sessionId) {
-        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + Browserstack.videoUrl(sessionId)
-                + "' type='video/mp4'></video></body></html>";
-    }
-
-    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
                 + getVideoUrl()
