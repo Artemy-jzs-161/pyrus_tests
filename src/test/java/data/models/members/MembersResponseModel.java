028c0b9fae6f9a91bac9b8fd3d81c7b637d03a60
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2024-07-04 21:39:20 +0700

    доработка теста api

--- src/test/java/data/models/members/MembersResponseModel.java
+++ src/test/java/data/models/members/MembersResponseModel.java
@@ -1,10 +1,12 @@
 package data.models.members;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class MembersResponseModel {
+    @JsonProperty("members")
     List<MemberModel> memberModels;
 }
